e330d1af00d8877b9be8c63d5a79c9b2ea0b154f
shinyuna <yunadev01@gmail.com>
2021-01-08 15:25:11 +0900

    Hash a password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 enum UserRole {
   Client,
@@ -25,4 +27,14 @@ export class User extends CoreEntity {
   @Column({ type: 'enum', enum: UserRole })
   @Field(type => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hashpassowrd(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log('🚀 ~ hashpassowrd ~ e', e);
+      throw new InternalServerErrorException();
+    }
+  }
 }
