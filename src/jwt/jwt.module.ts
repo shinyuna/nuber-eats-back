e8f53761c57c4f4ff1f01047afa1d3c82bc8c173
shinyuna <yunadev01@gmail.com>
2021-01-13 15:54:24 +0900

    Generating JWT module

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,22 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { JwtService } from './jwt.service';
+import { CONFIG_OPTIONS } from './jwt.constants';
+
+@Module({})
+@Global()
+export class JwtModule {
+  static forRoot(options: JwtModuleOptions): DynamicModule {
+    return {
+      module: JwtModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+        JwtService,
+      ],
+      exports: [JwtService],
+    };
+  }
+}
