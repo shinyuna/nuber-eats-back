634f10bf47ff9e7fea2746e16343d5f8799c2589
shinyuna <yunadev01@gmail.com>
2021-01-13 15:54:47 +0900

    JWT Middleware

---
+++ src/jwt/jwt.middleware.ts
@@ -0,0 +1,26 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from './jwt.service';
+
+@Injectable()
+export class JwtMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService, private readonly usersService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt'];
+      try {
+        const decoded = this.jwtService.vertify(token.toString());
+        if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+          const user = await this.usersService.findById(decoded['id']);
+          req['user'] = user;
+        }
+      } catch (error) {
+        console.log('🚀 ~ use ~ error', error);
+      }
+    }
+    next();
+  }
+}
+// implements = class가 interface처럼 행동해야 한다.
