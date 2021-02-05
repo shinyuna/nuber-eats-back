74350147ffb67e90fe19290c4a0abb7876261ef4
신유나 <yunadev01@gmail.com>
2021-02-05 22:59:56 +0900

    🧩 Create Restaurant API - edit restaurant

--- src/restaurants/restaurant.service.ts
+++ src/restaurants/restaurant.service.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EditProfileOutput } from 'src/users/dtos/edit-profile.dto';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
-import { CreateRestaurantInput, CreateRestaurantOutput } from './dto/createRestaurant.dto';
+import { CreateRestaurantInput, CreateRestaurantOutput } from './dto/create-restaurant.dto';
+import { EditRestaurantInput } from './dto/edit-restaurant.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
@@ -13,19 +15,14 @@ export class RestaurantService {
     @InjectRepository(Category) private readonly categories: Repository<Category>,
   ) {}
 
-  async createRestaurant(
-    owner: User,
-    createRestaurantInput: CreateRestaurantInput,
-  ): Promise<CreateRestaurantOutput> {
+  async createRestaurant(owner: User, createRestaurantInput: CreateRestaurantInput): Promise<CreateRestaurantOutput> {
     try {
       const newRestaurant = this.restaurants.create(createRestaurantInput);
       const categoryName = createRestaurantInput.categoryName.trim().toLowerCase();
       const categorySlug = categoryName.replace(/ /g, '-');
       let category = await this.categories.findOne({ slug: categorySlug });
       if (!category) {
-        category = await this.categories.save(
-          this.categories.create({ name: categoryName, slug: categorySlug }),
-        );
+        category = await this.categories.save(this.categories.create({ name: categoryName, slug: categorySlug }));
       }
       newRestaurant.owner = owner;
       newRestaurant.category = category;
@@ -33,6 +30,16 @@ export class RestaurantService {
       return { ok: true };
     } catch (error) {
       return { ok: false, error: 'Could not create restaurant' };
+    }
+  }
+
+  async editRestaurant(owner: User, editRestaurantInput: EditRestaurantInput): Promise<EditProfileOutput> {
+    try {
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
     }
   }
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
-import { CreateRestaurantInput, CreateRestaurantOutput } from './dto/createRestaurant.dto';
+import { CreateRestaurantInput, CreateRestaurantOutput } from './dto/create-restaurant.dto';
 import { RestaurantService } from './restaurant.service';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { Role } from 'src/auth/role.decorator';
+import { EditRestaurantInput, EditRestaurantOutput } from './dto/edit-restaurant.dto';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
@@ -12,10 +13,16 @@ export class RestaurantResolver {
 
   @Mutation(returns => CreateRestaurantOutput)
   @Role(['Owner'])
-  async createRastaurant(
+  async createRestaurant(
     @AuthUser() authUser: User,
     @Args('input') params: CreateRestaurantInput,
   ): Promise<CreateRestaurantOutput> {
     return this.restaurantService.createRestaurant(authUser, params);
+  }
+
+  @Mutation(returns => EditRestaurantOutput)
+  @Role(['Owner'])
+  async editRestaurant(@Args('input') params: EditRestaurantInput): Promise<EditRestaurantOutput> {
+    return { ok: true };
   }
 }
