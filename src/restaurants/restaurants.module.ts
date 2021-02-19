2f6cf9e5d7aa4976fcebefd69a4211c33891dca4
신유나 <yunadev01@gmail.com>
2021-02-20 00:13:43 +0900

    Update restaurant module - inject dish for using the repository

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repositories';
 import { CategoryService, RestaurantService } from './restaurant.service';
-import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
+import { CategoryResolver, DishResolver, RestaurantResolver } from './restaurants.resolver';
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])],
-  providers: [RestaurantResolver, RestaurantService, CategoryResolver, CategoryService],
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository, Dish])],
+  providers: [RestaurantResolver, RestaurantService, CategoryResolver, CategoryService, DishResolver],
 })
 export class RestaurantsModule {}
